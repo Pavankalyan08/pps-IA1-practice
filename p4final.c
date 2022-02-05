b3164742a9c590fb36408df4b1e005c0d4981f31
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-02-05 14:29:14 +0000

    p4final

---
+++ p4final.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int a;
+  printf("enter the size of number\n");
+  scanf("%d",&a);
+  return a;
+}
+void input_array(int n,int a[n])
+{
+  int i;
+  printf("enter the numbers\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+}
+int sum_n_arrays(int n,int a[n])
+{
+  int i,sum=0;
+  for(i=0;i<n;i++)
+  {
+    sum=sum+a[i];
+  }
+  return sum;
+}
+void out_put(int n,int a[n],int sum)
+{
+  int i;
+  for(i=0;i<n-1;i++)
+  {
+    printf("%d+",a[i]);
+  }
+  printf("%d is %d",a[i],sum);
+}
+int main()
+{
+  int n=input_array_size();
+  int a[n];
+  input_array(n,a);
+  int sum=sum_n_arrays(n,a);
+  out_put(n,a,sum);
+  return 0;
+}
